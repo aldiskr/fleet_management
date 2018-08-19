3a3e4edfb523f48315e5a409415e5437936b55e6
Aldis <aldiskrauze@gmail.com>
2018-08-19 18:53:33 +0300

    update

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,11 +1,19 @@
 class Job < ApplicationRecord
   belongs_to :driver
   belongs_to :car
-  validate :busy_from_more_than_busy_to
+  validate :busy_from_greater_than_busy_to
+  validate :job_date_time_overlaps
+  validates :busy_to, :busy_from, :driver, :car, presence: true
 
-  def busy_from_more_than_busy_to
-    if busy_from > busy_to
-      errors.add(:busy_to, "Busy to needs to be bigger than Busy from")
+  def busy_from_greater_than_busy_to
+    if busy_from >= busy_to
+      errors.add(:busy_to, " needs to be greater than Busy from")
+    end
+  end
+
+  def job_date_time_overlaps
+    if car.jobs.where('busy_from <= ? AND busy_to >= ?', busy_to, busy_from).count > 0
+      errors.add(:car, " is taken during selected period")
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+drivers = Driver.create([{name: 'John'}, {name: 'Peter'}])
+cars = Car.create([{name: 'Audi'},{name: 'BMW'}])
